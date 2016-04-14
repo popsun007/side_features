c5aa9626ea86a0ae743fa0c10f5ed0ac61e969a9
Frank Sun <popsun2010@gmail.com>
2016-04-13 22:40:06 -0700

    Update Jquery method for Masonry

--- masonry/index.php
+++ masonry/index.php
@@ -2,11 +2,19 @@
 <html>
 <head>
 	<title>Masonry</title>
-	<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.grid').masonry({
+			  itemSelector: '.grid-item',
+			  columnWidth: 10
+			});
+		});
+	</script>
 </head>
 <body>
-<div class="grid" data-masonry='{ "columnWidth": 10, "itemSelector": ".grid-item" }'>
+<!-- <div class="grid" data-masonry='{ "columnWidth": 10, "itemSelector": ".grid-item" }'> -->
 <div class="grid">
   <div class="grid-item"></div>
   <div class="grid-item grid-item--width2"></div>
@@ -25,5 +33,7 @@
   <div class="grid-item grid-item--width2"></div>
   <div class="grid-item grid-item--height2"></div>
 </div>
+
+<script src="masonry.pkgd.min.js"></script>
 </body>
 </html>
