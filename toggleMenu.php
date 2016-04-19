d22c78ecc2629d37764bb44ec38e5069b188a9e3
Frank Sun <popsun2010@gmail.com>
2016-04-18 23:01:19 -0700

    Toggle menu

---
+++ toggleMenu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+	<nav id="mobile">
+	 
+	    <div id="toggle-bar">
+	        <strong><a class="mtoggle" href="#">MAIN MENU</a></strong>
+	        <a class="navicon mtoggle" href="#">MAIN MENU</a>
+	    </div>
+	 
+	    <ul id="mmenu">
+	        <li><a href="#">Home</a></li>
+	        <li><a href="#">Products</a>
+	            <ul>
+	                <li><a href="#">HTML</a></li>
+	                <li><a href="#">CSS</a></li>
+	                <li><a href="#">Javascript</a>
+	                    <ul>
+	                        <li><a href="#">jQuery</a></li>
+	                        <li><a href="#">MooTools</a></li>
+	                    </ul>
+	            	</li>
+	            </ul>
+	        </li>
+	        <li><a href="#">About</a></li>
+	        <li><a href="#">Contact</a></li>       	
+	    </ul>
+	</nav>
+</body>
+</html>
